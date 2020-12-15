2634571ff69f5ef1ab2ef2835e1aee96d5941fa7
GGBoyCode <1770061644@qq.com>
2020-12-15 20:57:46 +0800

    新增修改密码接口

---
+++ src/com/servlet/user/UpdatePasswordServlet.java
@@ -0,0 +1,49 @@
+package com.servlet.user;
+
+import com.alibaba.fastjson.JSONObject;
+import com.entity.User;
+import com.service.IUserService;
+import com.service.Impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+@WebServlet("/user/update/password")
+public class UpdatePasswordServlet extends HttpServlet {
+    private IUserService userService = new UserServiceImpl();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean isSuccess = true;
+        //获取密码
+        String password = request.getParameter("password");
+        User user = new User("18180443372", password);
+
+        try {
+            isSuccess = userService.UpdatePassword(user);
+        } catch (SQLException e) {
+            isSuccess = false;
+            e.printStackTrace();
+        } catch (Exception e) {
+            isSuccess = false;
+            e.printStackTrace();
+        } finally {
+            JSONObject jsonObject = new JSONObject();
+            if(isSuccess) {
+                jsonObject.put("code", 20000);
+            } else {
+                jsonObject.put("code", 10000);
+            }
+            PrintWriter out = response.getWriter();
+            out.print(jsonObject);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
