9f9ad17b877302ffa19ee5eaafd2574f3312fc11
GGBoyCode <1770061644@qq.com>
2020-12-16 11:46:20 +0800

    新增商品帮助类

---
+++ src/com/util/WareUtil.java
@@ -0,0 +1,66 @@
+package com.util;
+
+import com.entity.Ware;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.lang.RandomStringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.List;
+
+public class WareUtil {
+    //文件夹名称
+    private static final String DIRNAME = "ware";
+
+    //上传图片
+    public static Ware upload(HttpServletRequest request) throws Exception {
+        Ware ware = new Ware();
+        //检查是否为上传项
+        if(ServletFileUpload.isMultipartContent(request)) {
+            List<FileItem> fileItems = Util.getFileItems(request, DIRNAME);
+            //若fileItems不为空且大小不为0 则进行下一步处理
+            if(fileItems != null && fileItems.size() > 0) {
+                //遍历fileItems数组
+                for(FileItem item:fileItems) {
+                    //判断是否为普通字段 若不为普通字段则为文件字段
+                    if(!item.isFormField()) {
+                        //获取文件名称
+                        //item.getName()可能得到“路径”+“文件名”通过new File()
+                        //转换为后再通过getName()可以得到真正的文件名
+                        String filename = new File(item.getName()).getName();
+                        int pointIndex = filename.lastIndexOf(".");
+                        //获取filename后缀名
+                        String suffix = filename.substring(pointIndex + 1);
+                        //重命名 在后面加8个随机字符串防止名字重复
+                        filename = filename.substring(0, pointIndex) + RandomStringUtils.randomAlphanumeric(8) + "." + suffix;
+                        //设置轮播图路径
+                        ware.setUrl(Util.UPLOAD_PATH + "/" + DIRNAME + "/" + filename);
+                        //检验上传是否为图片
+                        if(suffix.matches("jpg|jpeg|png|")) {
+                            //文件位置
+                            String filepath = Util.Path + DIRNAME + File.separator + filename;
+                            File file = new File(filepath);
+                            //将文件写入服务器硬盘
+                            item.write(file);
+                        } else {
+                            return null;
+                        }
+                    } else {
+                        if("id".equals(item.getFieldName())) {
+                            //获取id
+                            ware.setId(Integer.parseInt(item.getString()));
+                        } else if("price".equals(item.getFieldName())) {
+                            //获取price
+                            ware.setPrice(Integer.parseInt(item.getString()));
+                        } else if("description".equals(item.getFieldName())) {
+                            ware.setDescription(item.getString());
+                        }
+                    }
+                }
+            }
+        }
+
+        return ware;
+    }
+}
